42250eb5447cbc020d5e13c6189ae9522acfd365
cn33303_李金媛 <jinyuan.li@powersi.com>
2022-02-25 14:30:10 +0800

    Initial commit

---
+++ service/service_hosp/src/main/java/com/lijy23/yygh/HospServiceApplication.java
@@ -0,0 +1,18 @@
+package com.lijy23.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author jinyuan.li
+ * @date 2022/1/27 17:00
+ * @Description:
+ */
+@SpringBootApplication
+public class HospServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HospServiceApplication.class,args);
+    }
+}
+
